3e1bebac7b92a5ec2ae32c9761c6da3e0dabd810
Nathan Dane <nathan@nathandane.co.uk>
2017-06-26 09:46:09 +0100

    Added presentation logic

--- SupportKit/SupportKit/SupportKit.swift
+++ SupportKit/SupportKit/SupportKit.swift
@@ -10,12 +10,46 @@ import Foundation
 import UIKit
 
 struct SupportKit {
+  // MARK: - Types
+  
+  typealias Callback = ((_ issue: SupportIssue) -> Void)
+  
+  // MARK: - Private Members
+  
   private var issues: [SupportIssue] = []
+  
+  // MARK: - Public Members
+  
   public var title = "Support Requested"
   public var message = "Support Message"
+  
+  // MARK: - Issue management
   
   public mutating func add(_ issue: SupportIssue) {
     issues.append(issue)
   }
   
+  public mutating func add(_ issues: [SupportIssue]) {
+    self.issues.append(contentsOf: issues)
+  }
+  
+  // MARK: - Presentation
+  
+  public func present(from: UIViewController, _ callback: Callback? = nil) {
+    let alertController = UIAlertController(title: title,
+                                            message: message,
+                                            preferredStyle: .actionSheet)
+    
+    for issue in issues {
+      let action = UIAlertAction(title: issue.title,
+                                 style: .default,
+                                 handler: { action in
+        callback?(issue)
+      })
+        
+      alertController.addAction(action)
+    }
+    
+    from.present(alertController, animated: true)
+  }
 }
