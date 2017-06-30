222c2c4ad2a99df535e9fac87ef839a8db37edf9
Andrew Jennings <andrew.jennings21@yahoo.co.uk>
2017-06-30 10:28:35 +0100

    Update BugIssue.swift
    Updating the default body text.

--- SupportKit/SupportKit/Types/BugIssue.swift
+++ SupportKit/SupportKit/Types/BugIssue.swift
@@ -11,7 +11,7 @@ import Foundation
 public struct BugIssue: SupportIssue {
   public let title = "Report a bug"
   public let subject = "I would like to report a bug"
-  public let body = "Bug report body"
+  public let body = "Sorry that you’re having an issue with your app.\nPlease complete the required information below to help report the bug:\n\n- Date of Issue seen\n- Device Issue was seen on (including model + operating system [e.g. iOS 10…])\n- Steps to reproduce (if requires specific actions)\n\nThank you for letting us know!"
   
   public init() {}
 }
